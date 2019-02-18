0add1df5e13d1083b3eff75f05ea329c2b4d9708
Jokin <327928971@qq.com>
2019-02-18 16:33:12 +0800

    update add more apps

--- asm.py
+++ asm.py
@@ -10,23 +10,14 @@ import requests
 from contextlib import closing
 
 
-def main():
-    # 检查目录情况
-    if not (os.path.isdir('./public')):
-        try:
-            os.mkdir('./public')
-        except Exception:
-            sys.exit('Failed to make public directory.')
-    # 关闭证书验证
-    ssl._create_default_https_context = ssl._create_unverified_context
-    # 取消关闭验证安全提醒
-    urllib3.disable_warnings()
+# 从GitHub获取下载链接
+def getDownloadLink(author, project, filename):
     # 获取下载页面
-    download_page = 'https://github.com/jokin1999/SMMS_Uploader/releases/latest'
+    download_page = 'https://github.com/{}/{}/releases/latest'.format(author, project)
     ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) WallpaperBackuper/1.0.0'
     http = urllib3.PoolManager()
     try:
-        print('Get: download_page')
+        print('Get: %s/%s' % (author, project))
         r = http.request(
             'get',
             download_page,
@@ -34,10 +25,18 @@ def main():
     except Exception:
         sys.exit('Failed to get download page.')
     data = r.data.decode('utf-8')
-    match = re.search('/jokin1999.+gui\.exe', data)
+    pattern = '/' + author + '.+' + filename.replace('.', '\\.')
+    print('search: ' + pattern)
+    match = re.search(pattern, data)
     if (match is None):
         sys.exit('Failed to search download link')
     download_link = 'https://github.com' + match.group(0)
+    return download_link
+
+
+def download_file(download_link, target='./public/'):
+    filename = os.path.basename(download_link)
+    print('Downloading: ' + filename)
     try:
         print('Get: file @ ' + download_link)
         # headers = {'user-agent': ua}
@@ -48,38 +47,54 @@ def main():
             content_size = int(res.headers['Content-Length'])
             print('File size: ' + str(content_size))
             try:
-                with open('./public/' + os.path.basename(download_link), "wb") as file:
+                with open(target + filename, "wb") as file:
                     for data in res.iter_content(chunk_size=chunk_size):
                         downloaded_size += chunk_size
-                        # print('Progress: ' + str(round(downloaded_size/content_size*100, 2)) + r'%', end='\r')
+                        print('Progress: ' + str(round(downloaded_size/content_size*100, 2)) + r'%', end='\r')
                         file.write(data)
             except Exception:
                 sys.exit('Failed to write file.')
     except Exception as e:
         sys.exit('Failed to download file.\n' + str(e))
-    file_gui = '/gui.exe'
-    # 上传CDN
-    # post_url = 'https://transfer.sh/'
-    # file = './gui.exe'
-    # print('Reading file.')
-    # try:
-    #     with open(file, 'rb') as f:
-    #         fileData = f.read()
-    # except Exception:
-    #     sys.exit('Failed to read file.')
-    # file = {file: fileData}
-    # print('Posting file.')
-    # r = requests.post(post_url, headers={'Max-Days': '2'}, files=file)
-    # file_gui = r.text
-    # print(file_gui)
-    # 修改HTML内容
+    return filename
+
+
+def allinone(author, project, filename, target='./public/'):
+    global filelist
+    download_link = getDownloadLink(author, project, filename)
+    filename = download_file(download_link, target)
+    tpl = '<a href="/__href__" target="_blank">__project__</a>'
+    filelist = filelist + tpl.replace('__href__', filename).replace('__project__', project)
+
+
+def main():
+    # 下载列表
+    download_list = [
+        ('jokin1999', 'SMMS_Uploader', 'gui.exe'),
+        ('jokin1999', 'PacDownloader', 'updatepac.exe'),
+    ]
+    # 检查目录情况
+    if not (os.path.isdir('./public')):
+        try:
+            os.mkdir('./public')
+        except Exception:
+            sys.exit('Failed to make public directory.')
+    # 关闭证书验证
+    ssl._create_default_https_context = ssl._create_unverified_context
+    # 取消关闭验证安全提醒
+    urllib3.disable_warnings()
+    for fileinfo in download_list:
+        if (3 in fileinfo):
+            allinone(fileinfo[0], fileinfo[1], fileinfo[2], fileinfo[3])
+        else:
+            allinone(fileinfo[0], fileinfo[1], fileinfo[2])
     # 读取模板
     try:
         with open('./assets/index.html', 'r', encoding='utf-8') as f:
             html = f.read()
     except Exception:
         sys.exit('Failed to read asset file.')
-    html = html.replace('__gui.exe__', file_gui)
+    html = html.replace('__list__', filelist)
     time_link = 'http://nodetime.gearhostpreview.com/'
     try:
         time_prc = requests.get(time_link, verify=False)
@@ -96,4 +111,6 @@ def main():
 
 
 if __name__ == '__main__':
+    # 定义全局变量
+    filelist = ''
     main()
