da844637f8c907567c45a72ff934ee5bd05e7969
Jokin <327928971@qq.com>
2019-03-05 00:14:59 +0800

    update css & js

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,10 @@ window.onresize = function() {
   resize();
 }
 function resize() {
+  let height_offset = -40
   let win_height = document.body.offsetHeight;
   let header_height = document.getElementById('header').offsetHeight;
   let footer_height = document.getElementById('footer').offsetHeight;
   console.log(win_height - header_height - footer_height + "px")
-  document.getElementById('container').style.minHeight = win_height - header_height - footer_height + "px";
+  document.getElementById('container').style.minHeight = win_height - header_height - footer_height + height_offset + "px";
 }
